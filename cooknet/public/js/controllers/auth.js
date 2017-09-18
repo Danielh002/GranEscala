573d6e83c652d4843a384f698ce65e7088486a4b
Andres Lenis <lenisandres@hotmail.com>
2017-09-18 10:05:41 -0500

    agregado vista de registro de usuario

--- cooknet/public/js/controllers/auth.js
+++ cooknet/public/js/controllers/auth.js
@@ -1,6 +1,7 @@
 app.controller('AuthController', [ '$rootScope','$scope','$location','$localStorage','AuthService', function( $rootScope,$scope,$location,$localStorage,AuthService) {
 
     $scope.login = function() {
+        $scope.message=null;
         data={
             email:$scope.email,
             password:$scope.password
@@ -15,18 +16,26 @@ app.controller('AuthController', [ '$rootScope','$scope','$location','$localStor
             }
             else if(res.status==404){
                 console.log("usuario o contraseña incorrecta");
+                $scope.message="usuario o contraseña incorrecta";
             }
 
         },function(res){
 
             if(res.data && res.data.message=="user dont exist"){
                 console.log("usuario o contraseña incorrecta");
+                $scope.message="usuario o contraseña incorrecta";
             }
         });
     };
 
-    $scope.signup = function() {
-        
+    $scope.signUp = function() {
+        console.log("lel");
+      data={
+          email:$scope.email,
+          password:$scope.password,
+          username:$scope.username
+      }
+      console.log(data);
     };
 
     $scope.me = function() {
@@ -37,5 +46,15 @@ app.controller('AuthController', [ '$rootScope','$scope','$location','$localStor
         $scope.token=null;
         delete $localStorage.token;
     };
+
+    $scope.existMessage=function(){
+        console.log("vmd")
+        if($scope.message){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
     // $scope.token = $localStorage.token;
 }]);
--- cooknet/public/js/services/auth.js
+++ cooknet/public/js/services/auth.js
@@ -1,6 +1,8 @@
 app.service('AuthService',function($http){
     this.signIn=function(data,success,error){
         $http.post('./api/signIn', data).then(success).catch(error);
-        
+    }
+    this.signUp=function(data,success,error){
+        $http.post('./api/signUp',data).then(success).catch(error);
     }
 });
