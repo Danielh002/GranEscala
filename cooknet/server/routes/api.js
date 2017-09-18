255439805f8775a2a4f47f98f6aec265a2cd15f0
Andres Lenis <lenisandres@hotmail.com>
2017-09-18 13:49:28 -0500

    agregado verficacion de usuario y correos backend

--- cooknet/server/routes/api.js
+++ cooknet/server/routes/api.js
@@ -7,6 +7,8 @@ const auth=require('../middlewares/auth')
 
 api.post('/signIn',userCtrl.signIn)
 api.post('/signUp',userCtrl.signUp)
+api.post('/userExist',userCtrl.userExist)
+api.post('/emailExist',userCtrl.emailExist)
 api.get('/private',auth.isAuth,(req,res)=>{
     res.status(200).send({messages:"tienes acceso"})
 })
