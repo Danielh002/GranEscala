fda081f9ac90a07e3b31c0e1eef8df7a2110aaa8
Andres Lenis <lenisandres@hotmail.com>
2017-09-19 11:13:01 -0500

    correccion packege.json

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,11 +2,11 @@ var gulp=require('gulp')
 
 gulp.task('copy', function () {
     gulp.src('./bower_components/angular/angular.min.js')
-        .pipe(gulp.dest('./public/js/libraries'));
+        .pipe(gulp.dest('./cooknet/public/js/libraries'));
     gulp.src('./bower_components/angular-route/angular-route.min.js')
-        .pipe(gulp.dest('./public/js/libraries'));
+        .pipe(gulp.dest('./cooknet/public/js/libraries'));
     gulp.src('./bower_components/ngstorage/ngStorage.min.js')
-        .pipe(gulp.dest('./public/js/libraries'));
+        .pipe(gulp.dest('./cooknet/public/js/libraries'));
     gulp.src('./bower_components/bootstrap/dist/css/bootstrap.min.css')
-        .pipe(gulp.dest('./public/css/libraries'));
+        .pipe(gulp.dest('./cooknet/public/css/libraries'));
 });
