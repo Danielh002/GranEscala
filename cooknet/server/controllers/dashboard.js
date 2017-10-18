0adbe59af7406fa69a65891df38617e9160ff36c
Andres Lenis <lenisandres@hotmail.com>
2017-10-18 08:00:25 -0500

    peticiones de tendencias y recientes de los contactos en backend

---
+++ cooknet/server/controllers/dashboard.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const User=require('./../models/user')
+const Recipe=require('./../models/recipe')
+
+function trendings(req,res){
+    Recipe.find({},"_id title description user dateCreates").sort({likes:-1}).limit(10)
+    .then(recipes=>{
+        res.send(recipes);
+    })
+    .catch(err=>{
+        res.status(500).send({message:"error to find recipes"})
+    })
+}
+
+function recipesFollowing(req,res){
+    User.findOne({user:req.query.user})
+    .then(user=>{
+        Recipe.find({user:{ $in: [user.follow]}},"_id title description user dateCreates").sort({dateCreates:-1}).limit(10)
+        .then(recipes=>{
+            res.send(recipes)
+        })
+        .catch(err=>{
+            res.status(500).send({message:"error to find recipes"})
+        })
+    })
+    .catch(err=>{
+        res.status(500).send({message:"error to find user"})
+    })
+}
+
+module.exports={
+    trendings,
+    recipesFollowing
+}
