0c32220a102bb57a2ea2d0f9c79ff80b946b7ab0
Andres Lenis <lenisandres@hotmail.com>
2017-11-08 11:00:53 -0500

    subida de archivos al servidor

--- cooknet/server/controllers/recipe.js
+++ cooknet/server/controllers/recipe.js
@@ -2,6 +2,7 @@
 
 const mongoose=require('mongoose')
 const Recipe=require('./../models/recipe')
+const multer=require('multer')
 
 function createRecipe(req,res){
     console.log(req.body)
@@ -111,10 +112,37 @@ function getRecipesByUser(req,res){
     })
 }
 
+var storage = multer.diskStorage({ //multers disk storage settings
+    destination: function (req, file, cb) {
+        cb(null, './../public/uploads/')
+    },
+    filename: function (req, file, cb) {
+        var datetimestamp = Date.now();
+        cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1])
+    }
+});
+var upload = multer({ //multer settings
+                storage: storage
+            }).single('file');
+/** API path that will upload the files */
+
+function uploadImageRecipe(req,res){
+    console.log("entro")
+    upload(req,res,function(err){
+        if(err){
+            console.log(err)
+             res.json({error_code:1,err_desc:err});
+             return;
+        }
+         res.json({error_code:0,err_desc:null});
+    })
+}
+
 module.exports={
     createRecipe,
     getRecipeById,
     deleteRecipe,
     updateRecipe,
-    getRecipesByUser
+    getRecipesByUser,
+    uploadImageRecipe
 }
