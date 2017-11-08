744946e3966fe5ef2955e580374fa802e0d6a473
Andres Lenis <lenisandres@hotmail.com>
2017-11-07 22:55:31 -0500

    envio de correos para invitación

--- cooknet/server/controllers/invite.js
+++ cooknet/server/controllers/invite.js
@@ -1,5 +1,30 @@
 'use strcit'
 function inviteUser(req,res){
+    var nodemailer = require('nodemailer');
+    
+            var transporter = nodemailer.createTransport({
+              service: 'gmail',
+              auth: {
+                    user: "cooknetpontidev@gmail.com",
+                    pass: 'cooknetPont'
+                }
+                });
+            var html='<h1>'+req.user+' te invito a que hagas parte de <a href="cooknet.herokuapp.com">Cooknet</a></h1>';
+            var mailOptions = {
+              from: 'lenisandres5@gmail.com',
+              to: req.body.email,
+              subject: 'Invitacion a Cooknet',
+              html:html
+            };
+    
+            transporter.sendMail(mailOptions, function(error, info){
+            if (error) {
+                console.log(error);
+            }
+            else {
+            console.log('Email sent: ' + info.response);
+            }
+        });
     res.send({message:"the user was invited by email"})
 }
 
